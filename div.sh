f997e4395215fc5f5a0337e65b2399c0f67afba5
prajakta <prajaktapatil364@gmail.com>
2025-11-08 22:28:31 +0530

    division interview que

---
+++ div.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#number divisible by 3 and 5 but not divisible by 15
+
+#Author prajakta
+
+for i in {1..100}; do
+	if ( [ `expr $i % 3` == 0 ] || [ `expr $i % 5` == 0 ] ) && [ `expr $i % 15` != 0 ];
+	then echo $i
+	fi;
+done
+
